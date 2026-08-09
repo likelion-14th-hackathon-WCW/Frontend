fc19c0fd7a5fe0cbf32ba9639653c3dbfeab8887
Yudam Jung <udamee1025@gmail.com>
2026-08-10 01:45:40 +0900

    feat: Footer 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
