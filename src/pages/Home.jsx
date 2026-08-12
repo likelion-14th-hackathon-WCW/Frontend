22faa0e891fb0749f4dfeae0da59d142be402c78
Yudam Jung <udamee1025@gmail.com>
2026-08-12 13:28:38 +0900

    feat: 홈-시즌상징 (프리뷰 버전)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero.jsx'
+import SeasonalSymbols from '../components/SeasonalSymbols.jsx'
 import PopularCombinations from '../components/PopularCombinations.jsx'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SeasonalSymbols />
       <PopularCombinations />
     </>
   )
