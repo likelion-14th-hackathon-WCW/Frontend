e7dff223e2636908edc9fedf87f7b6de729009eb
Yudam Jung <udamee1025@gmail.com>
2026-08-19 02:31:20 +0900

    feat: footer-privacy policy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import './Footer.css'
 
 const FOOTER_LINKS = [
-  { label: '개인정보 처리방침', href: '#' },
+  { label: '개인정보 처리방침', href: '/privacy' },
   { label: '이용약관', href: '/terms' },
   { label: '장인정신', href: '/craftsmanship' },
   { label: '고객센터', href: '#' },
