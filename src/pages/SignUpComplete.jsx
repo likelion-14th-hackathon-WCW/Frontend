71e2532782597f05a88c4fbba6a1d7fae6c1aa2f
Yudam Jung <udamee1025@gmail.com>
2026-08-21 09:02:56 +0900

    fix: 회원가입 완료시 서비스명->Yeongyeol 변경

--- src/pages/SignUpComplete.jsx
+++ src/pages/SignUpComplete.jsx
@@ -11,7 +11,9 @@ export default function SignUpComplete() {
 
         <div className="signup-complete__message">
           <h1 className="signup-complete__title">회원가입이 완료되었습니다.</h1>
-          <p className="signup-complete__subtitle">(서비스명)의 회원이 되신 것을 환영합니다.</p>
+          <p className="signup-complete__subtitle">
+            <span className="signup-complete__subtitle-brand">Yeongyeol</span>의 회원이 되신 것을 환영합니다.
+          </p>
         </div>
 
         <div className="signup-complete__actions">
